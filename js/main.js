634602b376908bfbbc50dd8703a8cc3e74487da2
SaidahmadHosilov <saidahmadhosilov@gmail.com>
2021-10-03 14:23:12 +0500

    modal header

--- js/main.js
+++ js/main.js
@@ -17,3 +17,63 @@ window.addEventListener('scroll', function(){
         arrowTop.classList.remove('toTopActive');
     }
 })
+
+/* Modal-course */
+let headerBtn = document.querySelector('.font-header-btn');
+let modalCourse = document.querySelector('.modal-course');
+let closeModal = document.getElementById('close-modal-course')
+let showModalCourse = document.querySelector('#modal-course-btn');
+
+// create cover modal div and show modal
+headerBtn.addEventListener('click', function(){
+    let coverModalBg = document.createElement('div');
+    coverModalBg.classList.add('cover-body-modal');
+    document.querySelector('.modal-in').append(coverModalBg);
+    
+    modalCourse.classList.add('modal-course-active');
+    document.body.style.overflow = 'hidden';
+})
+
+// close modal
+closeModal.addEventListener('click', function(){
+    let coverModal = document.querySelector('.cover-body-modal');
+
+    modalCourse.classList.remove('modal-course-active');
+    document.querySelector('.modal-in').removeChild(coverModal);
+})
+
+// close modal with target
+window.addEventListener('click', function(event){
+    var modalTarget = event.target;
+    if(modalTarget.classList.contains('cover-body-modal')){
+        modalTarget.parentElement.removeChild(modalTarget);
+
+        modalCourse.classList.remove('modal-course-active');
+        modalSubmit.classList.remove('modal-submit-active');
+    }
+})
+
+// submit modal
+let closeSubmitBtn = document.querySelector('#close-submit-btn');
+let modalSubmit = document.querySelector('.modal-course-submit');
+
+showModalCourse.addEventListener('click', function(){
+    let coverModalBg = document.createElement('div');
+    coverModalBg.classList.add('cover-body-modal');
+    document.querySelector('.modal-in-submit').append(coverModalBg);
+
+    modalCourse.classList.remove('modal-course-active');
+    modalSubmit.classList.add('modal-submit-active');
+
+    document.querySelector('.modal-course .modal-in')
+    .removeChild(document.querySelector('.modal-course .cover-body-modal'));
+})
+
+closeSubmitBtn.addEventListener('click', function(){
+    modalSubmit.classList.remove('modal-submit-active');
+    document.querySelector('.modal-in-submit')
+    .removeChild(document.querySelector('.cover-body-modal'))
+})
+
+
+/* Modal-courses end */
