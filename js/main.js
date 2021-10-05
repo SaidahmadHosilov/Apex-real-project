20041cc6fef92d8e82cb80559aaff099d9b8f143
SaidahmadHosilov <saidahmadhosilov@gmail.com>
2021-10-05 09:30:37 +0500

    result part

--- js/main.js
+++ js/main.js
@@ -78,6 +78,44 @@ closeSubmitBtn.addEventListener('click', function(){
     document.body.style.overflow = 'auto';
 })
 
+// course part
+let courseBtn = document.querySelectorAll('.course-btn');
+for(const btn of courseBtn){
+    btn.addEventListener('click', function(){
+        let coverModalBg = document.createElement('div');
+        coverModalBg.classList.add('cover-body-modal');
+        document.querySelector('.modal-in').append(coverModalBg);
+        
+        modalCourse.classList.add('modal-course-active');
+        document.body.style.overflow = 'hidden';
+    })
+}
+// course part
+
+// Teacher part
+
+// modal img
+let images = document.querySelectorAll('#photos-div .photos img');
+let modalImgShow = document.querySelector('.img-modal');
+let modalImg = document.querySelector('.img-modal img');
+for(const img of images){
+    img.addEventListener('click', function(event){
+        let targetImgSrc = event.target.getAttribute('src');
+
+        modalImg.src = targetImgSrc;
+        modalImgShow.classList.add('img-modal-active');
+        document.body.style.overflow = 'hidden';
+    })
+}
+window.addEventListener('click', function(evt){
+    if(evt.target.classList == 'modal-img-main'){
+        modalImgShow.classList.remove('img-modal-active');
+        document.body.style.overflow = 'auto';
+    }
+})
+// modal img
+// Teacher part
+
 
 /* Modal-courses end */
 
@@ -85,8 +123,9 @@ closeSubmitBtn.addEventListener('click', function(){
 /* -- owl carousel -- */
 
 $(document).ready(function(){
+    autoPlayYouTubeModal();
     
-    // New Phones Carousel
+    // News Carousel
     $("#news-div .owl-carousel").owlCarousel({
         loop: true,
         nav: false,
@@ -109,4 +148,143 @@ $(document).ready(function(){
             },
         }
     })
+
+    // Courses Carousel
+    $("#courses-div .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 1
+            },
+            700:{
+                items: 2
+            },
+            950:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            },
+        }
+    })
+
+    // Teachers Carousel
+    $("#teachers-div .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 1
+            },
+            700:{
+                items: 2
+            },
+            950:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            },
+        }
+    })
+
+    // Videos Carousel
+    $("#videos-div .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 1
+            },
+            700:{
+                items: 2
+            },
+            950:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            },
+        }
+    })
+
+    // Results Carousel
+    $("#result-div .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 1
+            },
+            700:{
+                items: 2
+            },
+            950:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            },
+        }
+    })
 })
+
+/* Modal video  */
+
+let closeVideo = document.getElementById('modal-video-close');
+let videoModal = document.querySelector('.video-modal');
+
+closeVideo.addEventListener('click', function(){
+    let videoCtn = document.querySelector('.video-container');
+    let videoIframe = document.querySelector('.video-container iframe');
+
+    videoCtn.removeChild(videoIframe);
+    videoModal.classList.remove('video-modal-active');
+})
+
+let videosOverlay = document.querySelectorAll('#videos-div .ytp-large-play-button');
+
+for(const overlay of videosOverlay){
+    overlay.addEventListener('click', function(event){
+        console.log(event.target);
+        console.log(event.currentTarget);
+        console.log('success');
+    })
+}
+window.addEventListener('click', function(e){
+    console.log(e.target);
+    console.log(e.currentTarget);
+})
+
+/* Modal video  */
+
+function autoPlayYouTubeModal() {
+    var trigger = $('.trigger');
+    trigger.click(function(e) {
+    e.preventDefault();
+    var theModal = $(this).data("target");
+    var videoSRC = $(this).attr("src");
+    var videoSRCauto = videoSRC + "?autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal).on('hidden.bs.modal', function(e) {
+        $(theModal + ' iframe').attr('src', '');
+    });
+    });
+};
